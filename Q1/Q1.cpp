abc24a209d042f9ac569d594143a63a10f55eaea
A-DILEEP <dileepalla2003@gmail.com>
2024-08-06 22:11:27 +0530

    Sum of 2 numbers

---
+++ Q1/Q1.cpp
@@ -0,0 +1,30 @@
+// C++
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class solution{
+    public:
+    vector<int> twosum(vector<int>&v1,int target){
+        int size=v1.size();
+        for(int i=0;i<size;i++){
+            for(int j=1;j<size;j++){
+                if(v1[i]+v1[j]==target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    };
+};
+int main(){
+
+    vector<int> vector{21,2,4,7,0,4,12,32};
+    int target;
+    cout<<"Enter the target :";
+    cin>>target;
+
+    solution q1;
+    q1.twosum(vector, target);
+}
+    
