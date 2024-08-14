a928f72211de01b4cb8b1566593c9c16b6d64687
A-DILEEP <dileepalla2003@gmail.com>
2024-08-14 17:08:10 +0530

    Decimal number to roman numerals

--- Q3/Q3.cpp
+++ Q3/Q3.cpp
@@ -2,27 +2,28 @@
 using namespace std;
 int printRoman(int number)
 {
-    int num[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
-    string sym[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
-    int i=12;    
-    while(number>0)
+  int value=number;
+  int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+  string sym[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+  int i = 12;
+  while (number > 0)
+  {
+    int div = number / num[i];
+    number = number % num[i];
+    while (div--)
     {
-      int div = number/num[i];
-      number = number%num[i];
-      while(div--)
-      {
-        cout<<sym[i];
-      }
-      i--;
+      cout << "Roman numerical for " << value << " is :" << sym[i];
     }
-    return 0;
+    i--;
+  }
+  return 0;
 }
 
 int main()
 {
-    int number ;
-    cout<<"Enter the number :";
-    cin>>number;
-    printRoman(number);
-    return 0;
+  int number;
+  cout << "Enter the number :";
+  cin >> number;
+  printRoman(number);
+  return 0;
 }
