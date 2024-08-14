cff15d027cc36824487ee0570b9ece2f94c8e958
A-DILEEP <dileepalla2003@gmail.com>
2024-08-14 16:46:54 +0530

    number to numerical value

---
+++ Q3/Q3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int printRoman(int number)
+{
+    int num[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+    string sym[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+    int i=12;    
+    while(number>0)
+    {
+      int div = number/num[i];
+      number = number%num[i];
+      while(div--)
+      {
+        cout<<sym[i];
+      }
+      i--;
+    }
+    return 0;
+}
+
+int main()
+{
+    int number ;
+    cout<<"Enter the number :";
+    cin>>number;
+    printRoman(number);
+    return 0;
+}
